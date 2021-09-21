6bda342f6bbc3ee524e217921a9aac5af0220e92
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2021-09-20 19:26:04 -0700

    created spinner2

---
+++ spinner2.js
@@ -0,0 +1,50 @@
+let slashArr = ['|', '/', '-', '\\', '|'];
+let delayTime = 100;
+
+for (let i = 0; i < slashArr.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(`\r ${slashArr[i]}`);
+  }, delayTime += 200);
+}
+
+/*
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1700);
+
+*/
